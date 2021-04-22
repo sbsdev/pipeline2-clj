d28a8da27c24d50bcf66cb237b75dc7e7232cbc6
Christian Egli <christian@egli.cc>
2021-04-22 10:25:40 +0200

    Port the deps from the old client library

--- project.clj
+++ project.clj
@@ -3,5 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/data.codec "0.1.1"]
+                 [org.clojure/data.xml "0.0.8"]
+                 [org.clojure/data.zip "0.1.2"]
+                 [clj-time "0.15.1"]
+                 [pandect "0.5.4"]
+                 [crypto-random "1.2.0"]
+                 [clj-http "3.9.1"]
+                 [slingshot "0.9.0"]]
   :repl-options {:init-ns pipeline2-clj.core})
